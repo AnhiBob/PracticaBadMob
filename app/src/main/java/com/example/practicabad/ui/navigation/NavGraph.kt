fe39bdb60fb3c302d5c22c94b4a55dc88e62a03f
Mudrov Maksim <mudrov010203@gmail.com>
2026-03-04 11:52:45 +0300

    День-1: Добавлен переход на экран восстановления пароля

--- app/src/main/java/com/example/practicabad/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/example/practicabad/ui/navigation/NavGraph.kt
@@ -6,6 +6,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.practicabad.ui.screens.RegisterAccountScreen
 import com.example.practicabad.ui.screens.SignInScreen
+import com.example.practicabad.ui.screens.ForgotPasswordScreen
 
 @Composable
 fun NavGraph() {
@@ -26,12 +27,24 @@ fun NavGraph() {
             )
         }
 
-        composable("sign_up") {
-            RegisterAccountScreen(
-                onSignInClick = {
-                    navController.navigate("sign_in") {
-                        popUpTo("sign_up") { inclusive = true }
-                    }
+        composable("sign_in") {
+            SignInScreen(
+                onSignUpClick = {
+                    navController.navigate("sign_up")
+                },
+                onForgotPasswordClick = {
+                    navController.navigate("forgot_password")  // Добавь это
+                }
+            )
+        }
+        composable("forgot_password") {
+            ForgotPasswordScreen(
+                onBackClick = {
+                    navController.popBackStack()
+                },
+                onSendClick = {
+                    // TODO: перейти на экран OTP
+                    navController.navigate("otp_verification")
                 }
             )
         }
