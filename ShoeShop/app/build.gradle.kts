8f8be7b139ba60396d974d76b2eb0ffbf67833fd
Mudrov Maksim <mudrov010203@gmail.com>
2026-03-06 16:47:31 +0300

    День-2: Последние изменения

--- ShoeShop/app/build.gradle.kts
+++ ShoeShop/app/build.gradle.kts
@@ -42,6 +42,16 @@ android {
 }
 
 dependencies {
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation(platform("androidx.compose:compose-bom:2024.02.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -56,9 +66,9 @@ dependencies {
     implementation(libs.logging.interceptor)
     implementation(libs.navigation.compose)
     implementation(libs.gson)
-    implementation("androidx.core:core-splashscreen:1.2.0")
-    implementation("com.google.accompanist:accompanist-pager:0.28.0")
-    implementation("com.google.accompanist:accompanist-pager-indicators:0.28.0")
+    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.accompanist.pager)
+    implementation(libs.accompanist.pager.indicators)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
